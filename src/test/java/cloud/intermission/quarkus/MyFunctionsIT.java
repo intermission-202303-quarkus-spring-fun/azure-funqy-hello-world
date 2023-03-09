3e6678bf73e67c3c8ab3e041addd3155bd0ca273
Richard Scholz <richard.scholz@senacor.com>
2023-03-09 14:18:17 +0100

    Basic content, after setup

---
+++ src/test/java/cloud/intermission/quarkus/MyFunctionsIT.java
@@ -0,0 +1,8 @@
+package cloud.intermission.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MyFunctionsIT extends MyFunctionsTest {
+    // Run the same tests in native
+}
